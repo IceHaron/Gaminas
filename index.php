5250601c5c7d204f7022f08a0c2a2c8466e9517f
IceHaron <ice_haron@mail.ru>
2013-12-27 17:52:39 +0400

    Попытался намутить .htaccess

--- index.php
+++ index.php
@@ -1,5 +1,19 @@
 <?php
 
+class root {
+  var $path;
+  
+  function root() {
+    $address = $_SERVER['HTTP_HOST'];
+    $this->path = $address;
+  }
+  
+}
+
+$page = new root();
+$source = 'http://' . $page->path . '/source';
+var_dump($source);
+
 require_once('/php/index.php');
 INCLUDE('/html/index.html');
 
--- php/index.php
+++ php/index.php
@@ -1,10 +1,12 @@
 <?php
+if (isset($_POST['token'])) {
 $s = file_get_contents('http://ulogin.ru/token.php?token=' . $_POST['token'] . '&host=' . $_SERVER['HTTP_HOST']);
 $user = json_decode($s, true);
 //$user['network'] - соц. сеть, через которую авторизовался пользователь
 //$user['identity'] - уникальная строка определяющая конкретного пользователя соц. сети
 //$user['first_name'] - имя пользователя
 //$user['last_name'] - фамилия пользователя
+}
 // var_dump($_COOKIE);
 if (isset($_COOKIE['uid']) && $_COOKIE['uid'] !== '') $uid = $_COOKIE['uid'];
 else $uid = 0;
@@ -14,16 +16,17 @@ if ($uid !== 0) {
 
   // Profile
   $profile_str = file_get_contents('http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=0BE85074D210A01F70B48205C44D1D56&steamids=' . $uid);
+  // $profile_str = '{"response":{"players":[{"personaname":"Dummy","profileurl":"gaminas.ice","avatar":"http://placehold.it/32x32"}]}}';
   $str .= '{"profile":' . $profile_str . ',';
 
   // Library
   $lib_str = file_get_contents('http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=0BE85074D210A01F70B48205C44D1D56&steamid=' . $uid . '&format=json&include_appinfo=1');
-  // var_dump($lib_str);
+  // $lib_str = '{"response":{"games":[]}}';
   $str .= '"lib":' . $lib_str . '';
 
   // Inventory
   $inv_str = file_get_contents('http://steamcommunity.com/profiles/' . $uid . '/inventory/json/753/1');
-  // var_dump($inv_str);
+  // $inv_str = '{}';
   $str .= '}';
 }
 
