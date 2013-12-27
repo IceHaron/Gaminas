cdbd7b02f6719bb9cf8a8f09ac81e72c8c71ce33
IceHaron <ice_haron@mail.ru>
2013-12-27 10:19:21 +0400

    Переработал основу, щас буду распиливать на макет и все остальное

---
+++ js/main.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+
+  $('#library_caption').click(function() {
+    $('#library').toggle();
+  });
+  
+  $('#logoff').click(function() {
+    var date = new Date(0);
+    document.cookie='uid=;expires=' + date.toUTCString();
+    window.location = '/';
+  });
+});
+
+function login(token){
+    $.getJSON("//ulogin.ru/token.php?host=" +
+        encodeURIComponent(location.toString()) + "&token=" + token + "&callback=?",
+    function(data){
+        data=$.parseJSON(data.toString());
+        if(!data.error){
+            document.cookie = 'uid=' + data.uid;
+            window.location.reload(); // Костыль
+        }
+    });
+}
---
+++ php/index.php
@@ -0,0 +1,38 @@
+<?php
+$s = file_get_contents('http://ulogin.ru/token.php?token=' . $_POST['token'] . '&host=' . $_SERVER['HTTP_HOST']);
+$user = json_decode($s, true);
+//$user['network'] - соц. сеть, через которую авторизовался пользователь
+//$user['identity'] - уникальная строка определяющая конкретного пользователя соц. сети
+//$user['first_name'] - имя пользователя
+//$user['last_name'] - фамилия пользователя
+// var_dump($_COOKIE);
+if (isset($_COOKIE['uid']) && $_COOKIE['uid'] !== '') $uid = $_COOKIE['uid'];
+else $uid = 0;
+
+if ($uid !== 0) {
+  $str = '';
+
+  // Profile
+  $profile_str = file_get_contents('http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=0BE85074D210A01F70B48205C44D1D56&steamids=' . $uid);
+  $str .= '{"profile":' . $profile_str . ',';
+
+  // Library
+  $lib_str = file_get_contents('http://api.steampowered.com/IPlayerService/GetOwnedGames/v0001/?key=0BE85074D210A01F70B48205C44D1D56&steamid=' . $uid . '&format=json&include_appinfo=1');
+  // var_dump($lib_str);
+  $str .= '"lib":' . $lib_str . '';
+
+  // Inventory
+  $inv_str = file_get_contents('http://steamcommunity.com/profiles/' . $uid . '/inventory/json/753/1');
+  // var_dump($inv_str);
+  $str .= '}';
+}
+
+// echo '<pre>';
+// echo $str;
+// echo '</pre>';
+$json = json_decode($str);
+$username = $json->profile->response->players[0]->personaname;
+$profurl = $json->profile->response->players[0]->profileurl;
+$avatar = $json->profile->response->players[0]->avatar;
+$gamearray = $json->lib->response->games;
+?>
