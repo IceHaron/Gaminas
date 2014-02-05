3dfd305a0f048aaa27c9c47102a684f9b79abf27
IceHaron <ice_haron@mail.ru>
2014-02-05 15:14:28 +0400

    (ВАЖНО) Поправил идиотские баги

--- php/write.php
+++ php/write.php
@@ -1,5 +1,5 @@
 <?
-global $GAMINAS;
+$rootfolder = isset($_SERVER['HOME']) ? $_SERVER['HOME'].'/gaminas' : $_SERVER['DOCUMENT_ROOT'];
 // Тырим XML, превращаем его в JSON и выдираем нужные нам системы
 $text = file_get_contents('https://api.eveonline.com/map/Jumps.xml.aspx');
 // http://eve-marketdata.com/developers/solarsystems.php ID систем
@@ -9,8 +9,8 @@ $arr = json_decode(json_encode($xml), TRUE);
 foreach($arr['result']['rowset']['row'] as $system) {
 	if ($system['@attributes']['solarSystemID'] == 30002187) $amarr = $system['@attributes']['shipJumps'];
 }
-$stars = json_decode(file_get_contents($GAMINAS['rootfolder'] . '/source/js/SolarSystem.js'), TRUE);
-$regions = json_decode(file_get_contents($GAMINAS['rootfolder'] . '/source/js/Region.js'), TRUE);
+$stars = json_decode(file_get_contents($rootfolder . '/source/js/SolarSystem.js'), TRUE);
+$regions = json_decode(file_get_contents($rootfolder . '/source/js/Region.js'), TRUE);
 echo '<pre>';
 // var_dump($stars);
 // var_dump($xml->result->rowset->row);
@@ -18,11 +18,11 @@ echo '<pre>';
 // $arr['result']['dataTime'] - time of query ETC
 echo '</pre>';
 
-$written = json_decode(file_get_contents($GAMINAS['rootfolder'] . '/source/txt/amarr.txt'), TRUE);
+$written = json_decode(file_get_contents($rootfolder . '/source/txt/amarr.txt'), TRUE);
 $written[ $arr['cachedUntil'] ] = $amarr;
 $write = json_encode($written);
 var_dump($write);
-$file = fopen($GAMINAS['rootfolder'] . '/source/txt/amarr.txt', 'w+b');
+$file = fopen($rootfolder . '/source/txt/amarr.txt', 'w+b');
 fwrite($file, $write);
 fclose($file);
 ?>
