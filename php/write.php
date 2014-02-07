88d85a0de64fa30aacfb1ab4ac2df09f9e5d8cfd
IceHaron <ice_haron@mail.ru>
2014-02-07 14:45:16 +0400

    (ВАЖНО) Вроде бы допилил сбор статистики

--- php/write.php
+++ php/write.php
@@ -1,5 +1,21 @@
 <?
 $rootfolder = isset($_SERVER['HOME']) ? $_SERVER['HOME'].'/gaminas' : $_SERVER['DOCUMENT_ROOT'];
+
+$host = 'localhost';
+$user = 'srv44030_gaminas';
+$pw = '230105';
+$base = 'srv44030_gaminas';
+
+// Подключаемся к базе
+$link = mysqli_connect($host, $user, $pw, $base);
+mysqli_set_charset($link, "utf8");
+
+$q = mysqli_query($link, 'SELECT SQL_CACHE * FROM activity_daily');
+if (!$q) echo mysqli_error($link);
+while ($row = mysqli_fetch_assoc($q)) {
+	$table[ $row['region'] ] = $row['activity'];
+}
+
 // Тырим XML, превращаем его в JSON и выдираем нужные нам системы
 $text = file_get_contents('https://api.eveonline.com/map/Jumps.xml.aspx');
 // http://eve-marketdata.com/developers/solarsystems.php ID систем
@@ -9,38 +25,59 @@ $arr = json_decode(json_encode($xml), TRUE);
 $stars = json_decode(file_get_contents($rootfolder . '/source/txt/systems.txt'), TRUE);
 $regions = json_decode(file_get_contents($rootfolder . '/source/txt/regions.txt'), TRUE);
 
-// foreach ($stars as $starID => $star) {
-	// $res[ $regions[ $star['regionID'] ] ][ $star['name'] ][ $arr['cachedUntil'] ] = '0';
-// }
+$cachetime = strtotime($arr['cachedUntil']);
 
-foreach ($regions as $id => $name) {
-	$written = json_decode(file_get_contents($rootfolder . '/source/txt/systemstats/' . $name . '.txt'), TRUE);
-	$res[ $name ] = $written;
+foreach ($stars as $starID => $star) {
+	$skeleton[ $regions[ $star['regionID'] ] ][ $starID ] = $star['name'];
+}
+
+foreach ($skeleton as $region => $systems) {
+	$written = json_decode($table[ $region ], TRUE);
+	if ($written) {
+		foreach($systems as $sysid => $system) {
+			$activity = isset($written[ $system ]) ? $written[ $system ] : array();
+			foreach ($activity as $ts => $jumps) {
+				$res[ $region ][ $system ][ $ts ] = $jumps;
+				$res[ $region ][ $system ][ $cachetime ] = '0';
+			}
+		}
+	} else {
+		foreach($systems as $sysid => $system) {
+			$res[ $region ][ $system ][ $cachetime ] = '0';
+		}
+	}
 }
 foreach($arr['result']['rowset']['row'] as $system) {
 	$sysid = $system['@attributes']['solarSystemID'];
 	$jumps = $system['@attributes']['shipJumps'];
-	$res[ $regions[ $stars[ $sysid ]['regionID'] ] ][ $stars[ $sysid ]['name'] ][ $arr['cachedUntil'] ] = $jumps;
+	$res[ $regions[ $stars[ $sysid ]['regionID'] ] ][ $stars[ $sysid ]['name'] ][ $cachetime ] = $jumps;
 }
-
-// echo '<pre>';
-// var_dump($res);
-// echo '</pre>';
 
 // $arr['currentTime'] - current time ETC
 // $arr['result']['dataTime'] - time of query ETC
 
 $trigger = array();
+$query_arr = array();
 
-foreach ($res as $region => $systemset) {
-$write = json_encode($systemset);
-$file = fopen($rootfolder . '/source/txt/systemstats/' . $region . '.txt', 'w+b');
-$wrote = fwrite($file, $write);
-if ($wrote === FALSE) $trigger[] = $region;
-fclose($file);
+foreach ($regions as $id => $region) {
+	$systemset = $res[ $region ];
+	$write = json_encode($systemset);
+	$file = fopen($rootfolder . '/source/txt/systemstats/' . $region . '.txt', 'w+b');
+	$wrote = fwrite($file, $write);
+	if ($wrote === FALSE) $trigger[] = $region;
+	fclose($file);
+	$query_arr[] .= "UPDATE `activity_daily` SET `activity` = '$write' WHERE `region` = '$region'; ";
 }
-if (count($trigger) == 0) { /*echo 'Writing stats for all regions successfull!';*/ }
-else foreach ($trigger as $reg) {
-	echo 'Writing stats for ' . $reg . ' failed!<br/>';
+
+// echo '<pre>';
+// var_dump($query_arr);
+// echo '</pre>';
+
+// echo 'Now: ' . $arr['currentTime'] . '(' . strtotime($arr['currentTime']) . ')<br/>
+			// Cache time: ' . $arr['cachedUntil'] . '(' . strtotime($arr['cachedUntil']) . ')';
+
+foreach ($query_arr as $query_str) {
+	mysqli_query($link, trim($query_str, ','));
+	echo mysqli_error($link);
 }
 ?>
