9bcf41059453210b1e8dfd88ed0b62eba7aa9923
IceHaron <ice_haron@mail.ru>
2014-02-20 16:15:24 +0400

    Убрал перевод со старой системы на новую

--- php/write.php
+++ php/write.php
@@ -87,9 +87,6 @@ foreach ($skeleton as $region => $systems) {
 			$monthlyactivity = isset($monthlywritten[ $system ]) ? $monthlywritten[ $system ] : array();		// Активность системы забираем в отдельную переменную
 			
 			foreach ($monthlyactivity as $date => $jumps) {
-				$monarr = array('01' => 'January', '02' => 'February', '03' => 'March', '04' => 'April', '05' => 'May', '06' => 'June', '07' => 'July', '08' => 'August', '09' => 'September', '10' => 'October', '11' => 'November', '12' => 'December');
-				$mon = substr($date, 3, 2);
-				$date = strtotime(preg_replace('/-\d\d-/', ' ' . $monarr[$mon] . ' ', $date));
 				$monthly[ $region ][ $system ][ $date ] = $jumps;															// В итоговый массив заталкиваем информацию о прошлых часах
 			}
 			
